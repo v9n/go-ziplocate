cf1a98ef555b26a9dcc9041a67a50be95f6e066d
kureikain <kureikain@gmail.com>
2014-08-26 17:12:15 -0700

    Router

---
+++ gozip.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "os"
+    "runtime"
+    "github.com/codegangsta/cli"
+    "github.com/kureikain/go-ziplocate/cmd"
+)
+
+const APP_VER = "0.0.1"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "gozip"
+	app.Usage = "Geocoder for ZIP"
+	app.Version = APP_VER
+	app.Commands = []cli.Command{
+		cmd.CmdWeb,
+		cmd.CmdImport,
+	}
+	app.Flags = append(app.Flags, []cli.Flag{}...)
+	app.Run(os.Args)
+}
