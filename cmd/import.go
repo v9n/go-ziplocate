6333b7f64c69b19f90b1bbf4fe61a65dbb7e876d
kureikain <kureikain@gmail.com>
2014-08-26 17:20:07 -0700

    Ready for command

---
+++ cmd/import.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"time"
+	"github.com/codegangsta/cli"
+)
+
+var CmdImport = cli.Command{
+	Name: "import",
+	Usage: "gozip -f shapefile",
+	Description: `Import shapefile into GoZip database which is backed by leveldb`,
+	Action: runImport,
+	Flags: []cli.Flag{},
+}
+
+func runImport(*cli.Context) {
+	log.Printf("Importing local repositories...%s", "tl_2014_us_zcta510.shp")
+
+	log.Println("Finish importing!")
+}
+
---
+++ cmd/web.go
@@ -0,0 +1,26 @@
+package cmd
+
+web (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"time"
+	"github.com/codegangsta/cli"
+)
+
+var CmdWeb = cli.Command{
+	Name: "web",
+	Usage: "web -p port",
+	Description: `Run API server on the specify port. default to 12385`,
+	Action: runWeb,
+	Flags: []cli.Flag{},
+}
+
+func runWeb(*cli.Context) {
+	log.Printf("Web runs on Port %d", 12385)
+
+	log.Println("Finish webing!")
+}
+
+
